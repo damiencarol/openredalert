6a81a418a58e40623b992040fc9ba72551eea74b
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-06-03 16:37:40 +0000

    Add license reference.

--- trunk/src/game/ActionEventQueue.cpp
+++ trunk/src/game/ActionEventQueue.cpp
@@ -1,3 +1,20 @@
+// ActionEventQueue.cpp
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "ActionEventQueue.h"
 
@@ -33,7 +50,7 @@ ActionEventQueue::~ActionEventQueue()
 }
 
 /** 
- * Schedules  event for later ececution.
+ * Schedules  event for later execution.
  * 
  * @param a class containing the action to run.
  */
