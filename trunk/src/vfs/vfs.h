137bcbb729765817cd6c57a61a179e2a70fbab09
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-06-18 15:57:00 +0000

    Add more comments.

--- trunk/src/vfs/vfs.h
+++ trunk/src/vfs/vfs.h
@@ -16,13 +16,16 @@ class MIXFiles;
 class VFSUtils
 {
 public:
+	/** Sets up externals so that the logger can work */
 	static void VFS_PreInit(const char* binpath);
 	static void VFS_Init(const char *binpath);
 	static void VFS_Destroy();
+	/** Try to load all archive of a specific game */
 	static void VFS_LoadGame(gametypes gt);
 	/** Most code just uses the VFS for reading, so this default to reading */
 	static VFile *VFS_Open(const char *fname);
 	static VFile *VFS_Open(const char *fname, const char* mode);
+	/** Close a VFile */
 	static void VFS_Close(VFile *file);
 	static const char* VFS_getFirstExisting(const std::vector<const char*>& files);
 	static const char* VFS_getFirstExisting(Uint32 count, ...);
