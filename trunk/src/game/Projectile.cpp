77fa29fb08c46c78980452ff51cf4720fd026699
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-06-23 18:04:22 +0000

    Add licence reference.

--- trunk/src/game/Projectile.cpp
+++ trunk/src/game/Projectile.cpp
@@ -1,3 +1,21 @@
+// Projectile.cpp
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "Projectile.h"
 
 #include <string>
@@ -49,7 +67,7 @@ Projectile::Projectile(string pname, ProjectileDataList* data, vector<SHPImage*>
 		
 		if (this->lnkProjectileData->getRotates()!= 0)
 		{
-			if (temp != NULL)
+			if (temp != 0)
 			{
 				rotationimgs = temp->getNumImg();
 			}
