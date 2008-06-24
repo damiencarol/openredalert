fc21c5ae2625c581fc8f4afb4df4061cd0cc8729
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-06-24 20:59:59 +0000

    Add licence reference and format code.

--- trunk/src/game/BarrelExplosionActionEvent.h
+++ trunk/src/game/BarrelExplosionActionEvent.h
@@ -1,8 +1,32 @@
+// BarrelExplosionActionEvent.h
+// 1.2
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef BARRELEXPLOSIONANIMEVENT_H
 #define BARRELEXPLOSIONANIMEVENT_H
 
 #include "ActionEvent.h"
 
+/**
+ * Animation of the barrels explosion
+ * @author Damien Carol (OpenRedAlert)
+ * @version 1.2
+ * @since r388 
+ */
 class BarrelExplosionActionEvent : public ActionEvent
 {
 public:
@@ -11,11 +35,14 @@ public:
 	 * @param pos The position of the explosion
 	 */
 	BarrelExplosionActionEvent(Uint32 p, Uint32 pos);
-	virtual ~BarrelExplosionActionEvent();
 	
     /** */
     virtual void run();
 private:
+
+    /**
+     * Position of the animation in the map 
+     */
 	Uint32 position;
 };
 
