e798f433ddb058cbe1851701f394f7bb0ad6dd5b
Damien Carol <damien.carol@gmail.com>
2008-06-25 19:20:42 +0000

    Add comments and licence reference.

--- src/game/NoActionTriggerAction.h
+++ src/game/NoActionTriggerAction.h
@@ -1,17 +1,41 @@
+// NoActionTriggerAction.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef NOACTIONTRIGGERACTION_H
 #define NOACTIONTRIGGERACTION_H
 
 #include "TriggerAction.h"
 
 /**
- * Action that do nothing. 
+ * Action that do nothing.
+ * 
+ * @author Damien Carol (OpenRedAlert)
+ * @version 1.0
+ * @since r379
  */
 class NoActionTriggerAction : public TriggerAction
 {
-public:    
-
+public:
+    /** Constructor */
     NoActionTriggerAction();
 
+    /** Execute the action */
     void execute();
 };
+
 #endif //NOACTIONTRIGGERACTION_H
