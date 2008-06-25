3aef824e719cdbda92deeb1263b062e412067ae7
Damien Carol <damien.carol@gmail.com>
2008-06-25 19:03:09 +0000

    Add comments and licence reference.

--- src/game/DoorAnimEvent.h
+++ src/game/DoorAnimEvent.h
@@ -1,3 +1,21 @@
+// DoorAnimEvent.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef DOORANIMEVENT_H
 #define DOORANIMEVENT_H
 
@@ -8,8 +26,13 @@ class Structure;
 
 /**
  * Animation used by the Weapons Factory to animate the door opening and closing
+ *
+ * @author Damien Carol (OpenRedAlert)
+ * @version 1.0
+ * @since r375
  */
-class DoorAnimEvent : public BuildingAnimEvent {
+class DoorAnimEvent : public BuildingAnimEvent 
+{
 public:
     /**
      * @param p the priority of this event
@@ -18,8 +41,10 @@ public:
      */
     DoorAnimEvent(Uint32 p, Structure* str, bool opening);
 	~DoorAnimEvent ();
+
     void anim_func(anim_nfo* data);
     void updateDamaged();
+
 private:
     Structure* strct;
 	Uint32 delayCounter;
