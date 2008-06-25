f97d9e665f72bc65698d8b42972ebc98eb7cb421
Damien Carol <damien.carol@gmail.com>
2008-06-25 20:13:21 +0000

    Create a new type for theater informations and methodes.

---
+++ src/game/MissionTheater.cpp
@@ -0,0 +1,60 @@
+// MissionTheater.cpp
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "MissionTheater.h"
+
+#include <string>
+
+using std::string;
+
+/** 
+ * @return the name of the theater 
+ */
+string MissionTheater::getName()
+{
+	// return the name of the theater
+	return name;
+}
+
+/**  */
+MissionTheater::MissionTheater()
+{
+}
+
+/**
+ * @param aName Name of the MissionTheater
+ */
+MissionTheater::MissionTheater(string aName)
+{
+	// Set the name
+	this->name = aName;
+
+	// Check if it's interior
+	if (this->name == "INTERIOR")
+	{
+		this->interior = true;
+	}
+}
+
+/** 
+ * @return <code>true</code> if it's the 'INTERIOR' theater 
+ */
+bool MissionTheater::isInterior()
+{
+	return this->interior;
+}
---
+++ src/game/MissionTheater.h
@@ -0,0 +1,52 @@
+// MissionTheater.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef MISSIONTHEATER_H
+#define MISSIONTHEATER_H
+
+#include <string>
+
+using std::string;
+
+/**
+ * Theater of a map
+ * 
+ * @author Damien Carol (OpenRedAlert)
+ * @version 1.0
+ * @since r340
+ */
+class MissionTheater
+{
+public:
+	/** Return the name of the theater */
+	string getName();
+	
+	/** Return true if it's interior */
+	bool isInterior();
+
+private:
+	MissionTheater();
+	/** Name of the theater */
+	string name;
+	bool interior;
+	
+protected:
+	MissionTheater(string name);
+};
+
+#endif //MISSIONTHEATER_H
