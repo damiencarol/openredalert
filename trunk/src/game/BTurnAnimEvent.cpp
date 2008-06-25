6fff9995a87b9819405570ce40b05a95fe0b874d
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-06-25 19:19:39 +0000

    Add comments and licence reference.

--- trunk/src/game/BTurnAnimEvent.cpp
+++ trunk/src/game/BTurnAnimEvent.cpp
@@ -1,3 +1,21 @@
+// BTurnAnimEvent.cpp
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "BTurnAnimEvent.h"
 
 #include <cmath>
@@ -5,6 +23,8 @@
 #include "anim_nfo.h"
 #include "Structure.h"
 
+/**
+ */
 BTurnAnimEvent::BTurnAnimEvent(Uint32 p, Structure* str, Uint8 face) : BuildingAnimEvent(p,str,6)
 {
     Uint8 layerface;
@@ -25,9 +45,10 @@ BTurnAnimEvent::BTurnAnimEvent(Uint32 p, Structure* str, Uint8 face) : BuildingA
     this->str = str;
 }
 
+/**
+ */
 void BTurnAnimEvent::anim_func(anim_nfo* data)
 {
-
     Uint8 layerface;
     layerface = (str->getImageNums()[0]&0x1f);
     if( abs((layerface-targetface)&0x1f) > abs(turnmod) ) {
