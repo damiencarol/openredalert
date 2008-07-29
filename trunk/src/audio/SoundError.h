caee7ea0c27ad9c7c596b9f99ab8607681de4aee
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-07-29 14:08:38 +0000

    Change license to GPLv2 in "audio" package.

--- trunk/src/audio/SoundError.h
+++ trunk/src/audio/SoundError.h
@@ -1,3 +1,19 @@
+// SoundError.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, version 2 of the License.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef SOUNDERROR_H
 #define SOUNDERROR_H
@@ -8,6 +24,8 @@
 using std::string;
 using std::runtime_error;
 
+/**
+ */
 class SoundError : public runtime_error
 {
 public:    
