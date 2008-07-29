bc484fc2e297432af4ec6ab1daedc1b24bd4650f
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-07-29 14:59:11 +0000

    Change license to GPLv2 in "misc" package.

--- trunk/src/misc/gametypes.h
+++ trunk/src/misc/gametypes.h
@@ -1,9 +1,27 @@
+// gametypes.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, version 2 of the License.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef GAMETYPES_H
 #define GAMETYPES_H
 
-enum gametypes {
-	GAME_TD = 1, 
-	GAME_RA = 2
+enum gametypes
+{
+    GAME_TD = 1,
+    GAME_RA = 2
 };
 
 #endif //GAMETYPES_H
