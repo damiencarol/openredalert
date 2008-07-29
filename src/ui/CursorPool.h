1d24d4dabffc40126492d8b575a534b9a4d050aa
Damien Carol <damien.carol@gmail.com>
2008-07-29 21:35:11 +0000

    Change license to GPLv2 in "ui" package.

--- src/ui/CursorPool.h
+++ src/ui/CursorPool.h
@@ -1,3 +1,20 @@
+// CursorPool.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, version 2 of the License.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef CURSORPOOL_H
 #define CURSORPOOL_H
 
@@ -66,15 +83,16 @@ class CursorInfo;
  no gold fix     213
  mini deploysupr 214-221
  */
-class CursorPool {
+class CursorPool
+{
 public:
-	CursorPool();
-	~CursorPool();
-	CursorInfo* getCursorByName(string name);
+    CursorPool();
+    ~CursorPool();
+    CursorInfo* getCursorByName(string name);
 
 private:
-	vector<CursorInfo*> cursorpool;
-	map<string, Uint16> name2index;
+    vector<CursorInfo*> cursorpool;
+    map<string, Uint16> name2index;
 };
 
 #endif //CURSORPOOL_H
--- src/ui/sidebarop.h
+++ src/ui/sidebarop.h
@@ -1,14 +1,32 @@
+// sidebarop.h
+// 1.0
+
+//    This file is part of OpenRedAlert.
+//
+//    OpenRedAlert is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, version 2 of the License.
+//
+//    OpenRedAlert is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef SIDEBAROP_H
 #define SIDEBAROP_H
 
-enum sidebarop {
-	sbo_null = 0,
-	sbo_build = 1,
-	sbo_scroll = 2,
-	sbo_unit = 4,
-	sbo_structure = 8,
-	sbo_up = 16,
-	sbo_down = 32
+enum sidebarop
+{
+    sbo_null = 0,
+    sbo_build = 1,
+    sbo_scroll = 2,
+    sbo_unit = 4,
+    sbo_structure = 8,
+    sbo_up = 16,
+    sbo_down = 32
 };
 
-#endif /* SIDEBAROP_H */
+#endif //SIDEBAROP_H
