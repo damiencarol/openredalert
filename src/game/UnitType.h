d3e33a37b9a2d85489286bb5f68ac44e2eed089e
Damien Carol <damien.carol@gmail.com>
2008-10-01 06:58:22 +0000

    Refactoring to use strings. Add comments.

--- src/game/UnitType.h
+++ src/game/UnitType.h
@@ -51,7 +51,7 @@ public:
 	Uint8 getType() const;
 	Uint16 *getSHPTNum();
 	const char* getTName() const;
-	const char* getName() const;
+	const string getName() const;
 
 	vector<char*> getOwners() const;
 	Uint8 getOffset() const;
@@ -75,7 +75,7 @@ public:
 
 	/** Returns the primary Weapon of this type of Unit */
 	Weapon* getWeapon() const;
-	/** Returns the primary Weapon of this type of Unit if
+	/** Returns the primary Weapon of this type of Unit if 
 	 * primary is true else returns the secondary Weapon */
 	Weapon* getWeapon(bool primary) const;
 
@@ -101,13 +101,6 @@ public:
 	void setInfiltrate(bool infiltrate);
 
 private:
-	/** Avoid copy */
-	UnitType(const UnitType& x);
-	/** Avoid implicit copy */
-	UnitType& operator=(const UnitType& x);
-
-
-
 	Uint32 *shpnums;
 	Uint16 *shptnum;
 	/** In multi player both sides can use this unit */
@@ -122,7 +115,7 @@ private:
 	Sint8 movemod;
 
 	char* tname;
-	char* name;
+	string name;
 	vector<char*> owners;
 
 	/** Talkback related members*/
@@ -145,13 +138,13 @@ private:
 	/** matches the unit's type name.*/
 	vector<UnitType*> specificTypeAllow;
 
-	/**
-	 * C4 = Equipped with building sabotage explosives
-	 * [presumes Infiltrate is true] (def=no)?
+	/** 
+	 * C4 = Equipped with building sabotage explosives 
+	 * [presumes Infiltrate is true] (def=no)? 
 	 */
 	bool c4;
 
-	/**
+	/** 
 	 * Infiltrate = Can it enter a building like a spy or thief (def=no)?
 	 */
 	bool infiltrate;
