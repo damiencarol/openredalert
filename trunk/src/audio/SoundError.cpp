0eca186a23b13a6dc1a5ed81b04dc71e8792cc51
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-10-01 09:01:46 +0000

    Change SoundError namespace

--- trunk/src/audio/SoundError.cpp
+++ trunk/src/audio/SoundError.cpp
@@ -23,7 +23,17 @@
 using std::string;
 using std::runtime_error;
 
+namespace OpenRedAlert
+{
+    
+namespace Sound
+{
+    
 SoundError::SoundError(const string& msg) :
 	std::runtime_error(msg)
 {
+}
+
+}
+
 }
