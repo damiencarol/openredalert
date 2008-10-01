ccf3c4db3827a18883cd3a6ddd86b6e5dc6d43b8
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2008-10-01 08:12:44 +0000

    Fix compilation bug due to svn:eol-style property

--- trunk/src/game/UnitAnimEvent.cpp
+++ trunk/src/game/UnitAnimEvent.cpp
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 // UnitAnimEvent.cpp
 // 1.0
 
@@ -67,74 +66,3 @@ void UnitAnimEvent::stopScheduled()
 void UnitAnimEvent::update()
 {
 }
-
-=======
-// UnitAnimEvent.cpp
-// 1.0
-
-//    This file is part of OpenRedAlert.
-//
-//    OpenRedAlert is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, version 2 of the License.
-//
-//    OpenRedAlert is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
-
-#include "UnitAnimEvent.h"
-
-#include <cmath>
-
-#include "SDL/SDL_types.h"
-#include "UnitOrStructure.h"
-#include "ActionEventQueue.h"
-#include "Unit.hpp"
-
-namespace p {
-	extern ActionEventQueue * aequeue;
-}
-
-UnitAnimEvent::UnitAnimEvent(Uint32 p, Unit* un) : ActionEvent(p)
-{
-    //logger->debug("UAE cons: this:%p un:%p\n",this,un);
-    this->un = un;
-    un->referTo();
-    scheduled = NULL;
-}
-
-UnitAnimEvent::~UnitAnimEvent()
-{
-    //logger->debug("UAE dest: this:%p un:%p sch:%p\n",this,un,scheduled);
-    if (scheduled != NULL) {
-        p::aequeue->scheduleEvent(scheduled);
-    }
-    un->unrefer();
-}
-
-void UnitAnimEvent::setSchedule(UnitAnimEvent* e)
-{
-    //logger->debug("Scheduling an event. (this: %p, e: %p)\n",this,e);
-    if (scheduled != NULL) {
-        scheduled->setSchedule(NULL);
-        scheduled->stop();
-    }
-    scheduled = e;
-}
-
-void UnitAnimEvent::stopScheduled()
-{
-    if (scheduled != NULL) {
-        scheduled->stop();
-    }
-}
-
-void UnitAnimEvent::update()
-{
-}
-
->>>>>>> .r571
