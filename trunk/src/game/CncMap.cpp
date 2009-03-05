d95f4055019b22e92786c8a57038d1186e21a6aa
damien.carol <damien.carol@c35b548a-094d-0410-b8a6-ad6cd10eeca8>
2009-03-05 07:54:29 +0000

    Fix bad include.

--- trunk/src/game/CncMap.cpp
+++ trunk/src/game/CncMap.cpp
@@ -22,7 +22,7 @@
 #include <vector>
 #include <stdexcept>
 
-#include <SDL\SDL_endian.h> // For use of SDL_SwapLE16()
+#include "SDL/SDL_endian.h" // For use of SDL_SwapLE16()
 
 #include "Player.h"
 #include "UnitAndStructurePool.h"
