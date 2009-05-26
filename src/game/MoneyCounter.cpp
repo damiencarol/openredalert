def957dc3a84b057fe9f1a76babc8c4a85aaf70c
Damien Carol <damien.carol@gmail.com>
2009-05-26 13:48:32 +0000

    Removed assert() call and deleted references to C libraries. Removed double license declaration.

--- src/game/MoneyCounter.cpp
+++ src/game/MoneyCounter.cpp
@@ -1,29 +1,10 @@
 // MoneyCounter.cpp
-// 1.4
-
+//
 //    This file is part of OpenRedAlert.
 //
 //    OpenRedAlert is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, version 2 of the License.
-//
-//    OpenRedAlert is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
-
-// MoneyCounter.cpp
-// 1.0
-
-//    This file is part of OpenRedAlert.
-//
-//    OpenRedAlert is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, version 2 of the License.
-
 //
 //    OpenRedAlert is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -34,8 +15,6 @@
 //    along with OpenRedAlert.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "MoneyCounter.h"
-
-#include <cassert>
 
 #include "SDL/SDL_types.h"
 
@@ -78,9 +57,6 @@ void MoneyCounter::run()
         creditsound = -1;
     }
     if (Ddebt > 0) {
-        if (!player->hasInfMoney()) {
-            assert(money - Ddebt >= 0);
-        }
         money -= Ddebt;
     } else if (sound && debitsound != -1) {
         pc::sfxeng->StopLoopedSound(debitsound);
